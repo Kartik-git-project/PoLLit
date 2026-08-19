62edb9477c660cb772890c1bc5ba0ed16eeeefca
Karitk Dhupar <kartiksoni@192.168.1.5>
2026-08-19 16:51:24 +0530

    Security fix: Move MongoDB URI to environment variables

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
  
 
 export const connectDB = async() => {
-    await mongoose.connect("process.env.MONGO_URI") 
+    console.log("MONGO_URI is:", process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI) 
         .then(() => {
             console.log("DB Connected!");
         })
